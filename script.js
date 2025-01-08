0c428d582d19b642789c39bf9942cd846b001c8f
CRoan3 <christophertroan@gmail.com>
2025-01-08 14:17:06 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//adds the "open" class to the hamburger-icon and .menu-links. We then style each version to have a different appearance  
+//querySelector queries the document for the given set of characters
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
